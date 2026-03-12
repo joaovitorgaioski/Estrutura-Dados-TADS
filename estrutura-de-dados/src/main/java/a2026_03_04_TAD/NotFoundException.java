c4d31fbacf24f26245c4f75aa9ee42dc88693200
João Vitor Gaioski <140276793+joaovitorgaioski@users.noreply.github.com>
2026-03-11 22:19:20 -0300

    Add files via upload

---
+++ estrutura-de-dados/src/main/java/a2026_03_04_TAD/NotFoundException.java
@@ -0,0 +1,8 @@
+package a2026_03_04_TAD;
+
+public class NotFoundException extends Exception{
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ estrutura-de-dados/src/main/java/a2026_03_04_TAD/OverflowException.java
@@ -0,0 +1,8 @@
+package a2026_03_04_TAD;
+
+public class OverflowException extends Exception{
+
+    public OverflowException(String message) {
+        super(message);
+    }
+}
